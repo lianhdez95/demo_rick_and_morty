b7cdd425990008e087beb0864c4c04469cb441eb
Lian Hernández <lianhdez95@gmail.com>
2024-06-13 19:44:53 -0400

    feat: Add local character database implementation and repository
    The code changes include the addition of a local character database implementation and repository. This implementation allows for searching characters locally and storing them in shared preferences. The repository acts as an interface between the data source and the domain layer.
    
    Based on the recent user commits and repository commits, it seems that the developer is working on adding new functionality related to character data storage and retrieval.
    
    Please note that the commit message should be adjusted according to the repository's commit message conventions.

---
+++ lib/data/repository/local_characterdb_repository_impl.dart
@@ -0,0 +1,14 @@
+
+import 'package:demo_rick_and_morty/data/models/character_response_model.dart';
+import 'package:demo_rick_and_morty/domain/repository/local_character_repository.dart';
+
+import '../../domain/datasource/local_character_datasource.dart';
+
+class LocalCharacterdbRepositoryImpl implements LocalCharacterRepository{
+  final LocalCharacterDatasource localCharacterDatasource;
+  LocalCharacterdbRepositoryImpl({required this.localCharacterDatasource});
+  @override
+  Future<List<Character>> searchCharacters(String query, int page) {
+    return localCharacterDatasource.searchCharacters(query, page);
+  }
+}
